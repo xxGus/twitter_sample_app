a6eb3c769e4c5104e62106dbf087a60e542a1aea
Kyle Carlson <thekylecarlson@gmail.com>
2013-11-29 21:53:29 -0600

    javascript character count live update on micropost entry

--- app/assets/javascripts/microposts.js
+++ app/assets/javascripts/microposts.js
@@ -1,36 +1,22 @@
 $(function() {
 
-  // updateRemainingChars();
+    $('#micropost-text').change(updateRemainingChars);
+    $('#micropost-text').keyup(updateRemainingChars);
 
-  // $('textarea#micropost-text').on('keydown', function() {
-  //   updateRemainingChars();
-  // });
+    function updateRemainingChars() {
+        chars = 140 - $('#micropost-text').val().length;
+        el = $('span#character-count');
+        
+        el.text("Remaining: " + chars.toString());
 
-  // function updateRemainingChars() {
-  //   chars = document.getElementById('micropost-text').value.length;
-  //   $('span#character-count').text("Remaining: " + (140 - chars).toString());
-  // }
-
-  $("span#character-count").each(function() {
-      var $this = $(this);
-      var maxLength = parseInt($this.attr('maxlength'));
-      $this.attr('maxlength', null);
-      
-      var el = $("<span class=\"character-count\">" + maxLength + "</span>");
-      el.insertAfter($this);
-      
-      $this.bind('keyup', function() {
-          var cc = $this.val().length;
-          
-          el.text(maxLength - cc);
-          
-          if(maxLength < cc) {
-              el.css('color', 'red');
-          } else {
-              el.css('color', null);
-          }
-      });
-  });
-
-
+        if(chars < 1) {
+            el.css('color', 'red');
+        } 
+        else if (chars < 25) {
+            el.css('color', 'orange');
+        }
+        else {
+            el.css('color', 'black');
+        }
+    }
 });
